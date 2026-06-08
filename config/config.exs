9b22c367b60a28f373b47e83c4ee437739b36f00
Yannic Inselmann <yannic.inselmann@gmail.com>
2026-06-08 20:05:39 +0200

    feat: authentication

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :guitar_vault, :scopes,
+  user: [
+    default: true,
+    module: GuitarVault.Accounts.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :id,
+    schema_table: :users,
+    test_data_fixture: GuitarVault.AccountsFixtures,
+    test_setup_helper: :register_and_log_in_user
+  ]
+
 config :guitar_vault,
   ecto_repos: [GuitarVault.Repo],
   generators: [timestamp_type: :utc_datetime]
--- lib/guitar_vault_web/router.ex
+++ lib/guitar_vault_web/router.ex
@@ -1,6 +1,8 @@
 defmodule GuitarVaultWeb.Router do
   use GuitarVaultWeb, :router
 
+  import GuitarVaultWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule GuitarVaultWeb.Router do
     plug :put_root_layout, html: {GuitarVaultWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_scope_for_user
   end
 
   pipeline :api do
@@ -40,5 +43,33 @@ defmodule GuitarVaultWeb.Router do
       live_dashboard "/dashboard", metrics: GuitarVaultWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
+  end
+
+  ## Authentication routes
+
+  scope "/", GuitarVaultWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{GuitarVaultWeb.UserAuth, :require_authenticated}] do
+      live "/users/settings", UserLive.Settings, :edit
+      live "/users/settings/confirm-email/:token", UserLive.Settings, :confirm_email
+    end
+
+    post "/users/update-password", UserSessionController, :update_password
+  end
+
+  scope "/", GuitarVaultWeb do
+    pipe_through [:browser]
+
+    live_session :current_user,
+      on_mount: [{GuitarVaultWeb.UserAuth, :mount_current_scope}] do
+      live "/users/register", UserLive.Registration, :new
+      live "/users/log-in", UserLive.Login, :new
+      live "/users/log-in/:token", UserLive.Confirmation, :new
+    end
+
+    post "/users/log-in", UserSessionController, :create
+    delete "/users/log-out", UserSessionController, :delete
   end
 end
